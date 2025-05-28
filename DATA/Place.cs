e97f416a58510abbb23d224f4e8637dc0b7cefe2
Flandra1-s <flandrasylejmani40@gmail.com>
2025-05-28 22:27:05 +0200

    Shto modelin Place per vendndodhjet
    - Fusha: Id, Name, Description
    - Perdoret per te grupuar hotelet ose vendet sipas lokacionit

---
+++ DATA/Place.cs
@@ -0,0 +1,9 @@
+namespace ReservationSystem.API.Models
+{
+    public class Place
+    {
+        public int Id { get; set; }
+        public String name { get; set; }
+        public String Description { get; set; }
+    }
+}
